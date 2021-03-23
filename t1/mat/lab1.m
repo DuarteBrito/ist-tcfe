aa7d7887c9052a7d82220c39ba1f02b9bc4896f0
Duarte Brito acer <jo4o.treta@gmail.com>
2021-03-23 11:39:56 +0000

    criar csv's

--- t1/mat/lab1.m
+++ t1/mat/lab1.m
@@ -1,3 +1,8 @@
+close all
+clear all
+
+pkg load symbolic
+
 syms G1
 syms G2
 syms G3
@@ -48,8 +53,8 @@ res_malhas = subs(fm,{R1, R2, R3, R4,R5, R6, R7,Va, Kc, Kb, Id},{1.04944227714,2
 
 res_nos = subs(fn,{G1,G2, G3, G4, G5, G6, G7, Va, Kc, Kb, Id},{1/1.04944227714,1/2.06296295698, 1/3.07855037163, 1/4.04814283444, 1/3.03583837907, 1/2.01824745844, 1/1.04357678508, 5.07638677695, 8.10223845988, 7.26693007101, 1.0053213836});
 
-res_nos = double(res_nos)
-res_malhas = double(res_malhas)
+res_nos = double(res_nos);
+res_malhas = double(res_malhas);
 
 i1 = res_malhas(1);
 i2 = res_malhas(2);
@@ -60,3 +65,30 @@ i6 = res_malhas(3);
 i7 = res_malhas(3);
 
 disp([i1; i2; i3; i4; i5; i6; i7])
+
+fidMalhas = fopen("malhas.csv","w");
+fprintf(fidMalhas,"Ia,%f\n",res_malhas(1));
+fprintf(fidMalhas,"Ib,%f\n",res_malhas(2));
+fprintf(fidMalhas,"Ic,%f\n",res_malhas(3));
+fprintf(fidMalhas,"Id,%f\n",res_malhas(4));
+fclose(fidMalhas);
+
+fidNos = fopen("nos.csv","w");
+fprintf(fidNos,"V1,%f\n",res_nos(1));
+fprintf(fidNos,"V2,%f\n",res_nos(2));
+fprintf(fidNos,"V3,%f\n",res_nos(3));
+fprintf(fidNos,"V4,%f\n",res_nos(4));
+fprintf(fidNos,"V5,%f\n",res_nos(5));
+fprintf(fidNos,"V6,%f\n",res_nos(6));
+fprintf(fidNos,"V7,%f\n",res_nos(7));
+fclose(fidNos);
+
+fidCur = fopen("cur.csv","w");
+fprintf(fidCur,"I1,%f\n",i1);
+fprintf(fidCur,"I2,%f\n",i2);
+fprintf(fidCur,"I3,%f\n",i3);
+fprintf(fidCur,"I4,%f\n",i4);
+fprintf(fidCur,"I5,%f\n",i5);
+fprintf(fidCur,"I6,%f\n",i6);
+fprintf(fidCur,"I7,%f\n",i7);
+fclose(fidCur);
